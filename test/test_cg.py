d4e9df84ca3fffdd2cbd8e9ce00b5a8ad2281bbd
Matthias Zeller <m.zeller1901@gmail.com>
2022-03-13 01:08:21 +0100

    unittests: normalize b's

--- test/test_cg.py
+++ test/test_cg.py
@@ -19,38 +19,45 @@ class TestCG(unittest.TestCase):
 
     def test_vanilla_cg(self):
         b = np.random.randn(self.n)
-        xk = cg_vanilla(lambda x: self.A@x, b, np.zeros(self.n), 50)
+        b /= b.dot(b)**0.5
+        m = 50
+        xk = cg_vanilla(lambda x: self.A@x, b, np.zeros(self.n), m)
         np.testing.assert_almost_equal(xk, self.Ainv@b, decimal=8)
 
     def test_vanilla_pcg(self):
         b = np.random.randn(self.n)
+        b /= b.dot(b)**0.5
         m = 30
         eigs_pinv = np.concatenate((np.ones(self.n-m), 1/self.eigs[-m:]))
         Pinv = self.Q @ np.diag(eigs_pinv) @ self.Q.T
-        xk = cg_vanilla(lambda x: self.A@x, b, np.zeros(self.n), 50)
-        np.testing.assert_almost_equal(xk, self.Ainv@b, decimal=8)
         xk = pcg_vanilla(lambda x: self.A@x, lambda x: Pinv@x, b, np.zeros(self.n), 50)
         np.testing.assert_almost_equal(xk, self.Ainv@b, decimal=8)
 
     def test_mbcg(self):
         t = 10
         B = np.random.randn(self.n, t)
+        B /= (B * B).sum(0)
         Xk, _ = mbcg(lambda X: self.A@X, lambda X: X, B, np.zeros((self.n, t)), 50)
         np.testing.assert_almost_equal(Xk, self.Ainv@B)
+
+        # TODO test T returned by mbcg
 
     def test_torch(self):
         A = torch.from_numpy(self.A)
         Ainv = torch.from_numpy(self.Ainv)
         b = torch.randn(self.n)
+        b /= b.dot(b)**0.5
 
-        xk = cg_vanilla(lambda x: A @ x, b, torch.zeros(self.n), 50)
+        m = 50
+        xk = cg_vanilla(lambda x: A @ x, b, torch.zeros(self.n), m)
         torch.testing.assert_allclose(xk, Ainv@b)
-        xk = pcg_vanilla(lambda x: A @ x, lambda x: x, b, torch.zeros(self.n), 50)
+        xk = pcg_vanilla(lambda x: A @ x, lambda x: x, b, torch.zeros(self.n), m)
         torch.testing.assert_allclose(xk, Ainv@b)
 
         t = 10
         B = torch.randn(self.n, t)
-        Xk, _ = mbcg(lambda X: A @ X, lambda X: X, B, torch.zeros((self.n, t)), 50)
+        B /= (B * B).sum(0)
+        Xk, _ = mbcg(lambda X: A @ X, lambda X: X, B, torch.zeros((self.n, t)), m)
         torch.testing.assert_allclose(Xk, Ainv@B)
 
 
